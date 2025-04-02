02f6e8ed09ff409f391467b3981287079ad49807
Zhuz111 <bakytbek.zhuldyz@inbox.ru>
2025-04-02 22:29:53 +0600

    first commit

---
+++ lib/models/profile.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation/dart';
+part 'profile.g.dart';
+@JsinSeriaLizable()
+class Profile {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+  Profile({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  
+  })
+factory Profile.fromJson(Map<String, dynamic>json) =>_$ProfileFromJson(json)
+Map<String,dynamic>toJson() => => _$ProfileFromJson(this);
+} 
